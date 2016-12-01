5874287ec3a253d961e92817b439c977429d2f61
Matias Larsson <matiasnu@gmail.com>
2016-12-01 11:03:39 +0100

    Added AutoComplete component to the Dialog example.

--- docs/src/app/components/pages/components/Dialog/ExampleDialogDatePicker.js
+++ docs/src/app/components/pages/components/Dialog/ExampleDialogDatePicker.js
@@ -3,6 +3,7 @@ import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import DatePicker from 'material-ui/DatePicker';
+import AutoComplete from 'material-ui/Autocomplete';
 
 /**
  * Dialogs can be nested. This example opens a Date Picker from within a Dialog.
@@ -32,9 +33,9 @@ export default class DialogExampleDialogDatePicker extends React.Component {
 
     return (
       <div>
-        <RaisedButton label="Dialog With Date Picker" onTouchTap={this.handleOpen} />
+        <RaisedButton label="Dialog With Date Picker and AutoComplete" onTouchTap={this.handleOpen} />
         <Dialog
-          title="Dialog With Date Picker"
+          title="Dialog With Date Picker and AutoComplete"
           actions={actions}
           modal={false}
           open={this.state.open}
@@ -42,6 +43,20 @@ export default class DialogExampleDialogDatePicker extends React.Component {
         >
           Open a Date Picker dialog from within a dialog.
           <DatePicker hintText="Date Picker" />
+          <AutoComplete
+            animated={false}
+            floatingLabelText="AutoComplete animated: false"
+            hintText={"Search for 'test', please"}
+            dataSource={[{value: 1, text: "Test 1"}, {value: 2, text: "Test 2"}, {value: 3, text: "Test 3"}, {value: 4, text: "Test 4"}, {value: 5, text: "Test 5"}]}
+            filter={AutoComplete.caseInsensitiveFilter}
+          />
+          <AutoComplete
+            animated={true}
+            floatingLabelText="AutoComplete animated: true"
+            hintText={"Search for 'test', please"}
+            dataSource={[{value: 1, text: "Test 1"}, {value: 2, text: "Test 2"}, {value: 3, text: "Test 3"}, {value: 4, text: "Test 4"}, {value: 5, text: "Test 5"}]}
+            filter={AutoComplete.caseInsensitiveFilter}
+          />
         </Dialog>
       </div>
     );
